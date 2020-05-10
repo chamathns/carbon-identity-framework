c3949da63d769f1c2aeb79acc996876a9a984540
Chamath Samarawickrama <chamaths@wso2.com>
2020-05-10 16:25:44 +0530

    add feature manager OSGi service

---
+++ components/feature-mgt/org.wso2.carbon.identity.feature.mgt/src/main/java/org/wso2/carbon/identity/feature/mgt/internal/FeatureManagerComponent.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.feature.mgt.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.*;
+import org.wso2.carbon.identity.core.util.IdentityCoreInitializedEvent;
+import org.wso2.carbon.identity.feature.mgt.FeatureManager;
+import org.wso2.carbon.identity.feature.mgt.FeatureManagerImpl;
+
+/**
+ * OSGi declarative services component which handles registration and un-registration of feature management service.
+ */
+@Component(
+        name = "carbon.identity.feature.mgt.component",
+        immediate = true
+)
+public class FeatureManagerComponent {
+
+    private static Log log = LogFactory.getLog(FeatureManagerComponent.class);
+
+    /**
+     * Register Feature Manager as an OSGi service.
+     *
+     * @param componentContext OSGi service component context.
+     */
+    @Activate
+    protected void activate(ComponentContext componentContext) {
+
+        try {
+            BundleContext bundleContext = componentContext.getBundleContext();
+
+            bundleContext.registerService(FeatureManager.class, new FeatureManagerImpl(), null);
+            if (log.isDebugEnabled()) {
+                log.debug("Feature Manager bundle is activated.");
+            }
+        } catch (Throwable e) {
+            log.error("Error while activating FeatureManagerComponent.", e);
+        }
+    }
+
+    @Reference(
+            name = "identityCoreInitializedEventService",
+            service = IdentityCoreInitializedEvent.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetIdentityCoreInitializedEventService"
+    )
+    protected void setIdentityCoreInitializedEventService(IdentityCoreInitializedEvent identityCoreInitializedEvent) {
+        /* reference IdentityCoreInitializedEvent service to guarantee that this component will wait until identity core
+         is started. */
+    }
+
+    protected void unsetIdentityCoreInitializedEventService(IdentityCoreInitializedEvent identityCoreInitializedEvent) {
+        /* reference IdentityCoreInitializedEvent service to guarantee that this component will wait until identity core
+         is started. */
+    }
+}
